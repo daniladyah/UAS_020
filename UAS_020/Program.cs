d2d23e7b3eb674ab7e1f4b730b5278ae725c9306
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-01-09 17:53:22 +0700

    Add project files.

---
+++ UAS_020/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace double_linked_list
+{
+    class Node
+    {
+        public int NOIndukMurid;
+        public string NamaMurid;
+        public string Kelas;
+        public Node next;
+        public Node prev;
+    }
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()
+        {
+            int noinduk;
+            string ni;
+            string kls;
+            Console.Write("\nEnter the roll number of the student :");
+            noinduk = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student :");
+            ni = Console.ReadLine();
+            Console.Write("\nEnter the class of the student :");
+            kls = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.NOIndukMurid = noinduk;
+            newNode.NamaMurid = ni;
+            newNode.Kelas = kls;
+
+            if (START == null || noinduk <= START.NOIndukMurid)
+            {
+                if ((START != null) && (noinduk == START.NOIndukMurid))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.prev = null;
+                START = newNode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = START;
+                current != null && noinduk >= current.NOIndukMurid;
+                previous = current, current = current.next)
+            {
+                if (noinduk == current.NOIndukMurid)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            newNode.next = current;
+            newNode.prev = previous;
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            current.next = newNode;
+        }
+        public bool search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = START; current != null && rollNo != current.NOIndukMurid; previous = current, current = current.next) { }
+            return (current != null);
+        }
+        public bool dellNode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (search(rollNo, ref previous, ref current) == false)
+                return false;
+            if (current.next == null)
+            {
+                previous.next = null;
+                return true;
+            }
+            if (current == START)
+            {
+                START = START.next;
+                if (START != null)
+                    START.prev = null;
+                return true;
+            }
+            previous.next = current.next;
+            current.next.prev = previous;
+            return true;
+        }
+        public bool listEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
+        }
+        public void ascending()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is Empty");
+            else
+            {
+                Console.WriteLine("\nRecord in the ascending order of" + "roll number are:\n");
+                Node currentNode;
+                for (currentNode = START; currentNode != null; currentNode = currentNode.next)
+                    Console.Write(currentNode.NOIndukMurid + "" + currentNode.NamaMurid + "\n");
+            }
+        }
+        public void descending()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is Empty");
+            else
+            {
+                Console.WriteLine("\nRecord the descending order of" + "roll number are:\n");
+                Node currentNode;
+                for (currentNode = START; currentNode != null; currentNode = currentNode.next)
+                { }
+                while (currentNode != null)
+                {
+                    Console.Write(currentNode.NOIndukMurid + "" + currentNode.NamaMurid + "\n");
+                    currentNode = currentNode.prev;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkedList obj = new DoubleLinkedList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all record in ascending order of roll number");
+                    Console.WriteLine("4. View all record in descending order of roll number");
+                    Console.WriteLine("5. Search for a record in the list");
+                    Console.WriteLine("6. Exit\n");
+                    Console.WriteLine("Enter yor choice(1-6):");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNode();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is Empty");
+                                    break;
+                                }
+                                Console.Write("\nEnter the roll number of the student" + "whose record is to be deleted:");
+                                int rollNo = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.dellNode(rollNo) == false)
+                                    Console.WriteLine("Record not found");
+                                else
+                                    Console.WriteLine("Record with roll number" + rollNo + "deleted\n");
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.ascending();
+                            }
+                            break;
+                        case '4':
+                            {
+                                obj.descending();
+                            }
+                            break;
+                        case '5':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList id Empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the rol number of the student whoses rocord you want to search:");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number" + curr.NOIndukMurid);
+                                    Console.WriteLine("\nName:" + curr.NamaMurid);
+                                }
+
+                            }
+                            break;
+                        case '6':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
+                    }
+                }
+                catch (Exception )
+                {
+                    Console.WriteLine("Check for the values entered");
+                }
+            }
+        }
+    }
+}
+
+//2.algoritma yang dipake adalah double linked list
+//3.TOP pd algoritma stack menggunakan LIFO yang mana elment akhir akan yang menjadi pertama yang dikeluarkan
+//4.-rear, front
+//5. a. 5
+//b.20,15,25,30,32,31,35,48,50,70,65,67,66,69,90,98,94,99
